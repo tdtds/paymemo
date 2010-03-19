1f46d0dfb06799047be85a309b240fa614152023
TADA Tadashi <sho@katsuura.tdtds.jp>
2010-03-19 21:44:54 +0900

    added comma to numerics.

--- paymemo.js
+++ paymemo.js
@@ -4,6 +4,13 @@ paymemo.js
 Copyright (C) 2010 TADA Tadashi <t@tdtds.jp>
 You can modify and distribue this under GPL.
  */
+
+function addFigure(str) {
+	var num = new String(str).replace(/,/g, "");
+	while(num != (num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
+	return num;
+}
+
 $(function(){
 	$('.paymemo').each(function(){
 		var e = this;
@@ -13,9 +20,9 @@ $(function(){
 		$(e).children('form').children('input[name=db]').attr('value',db);
 		$.getJSON('paymemo.cgi?db='+db,function(json){
 			var table = $(e).children('table');
-			table.append('<caption>Total: '+json['total']+'</caption>');
+			table.append('<caption>Total: '+addFigure(json['total'])+'</caption>');
 			jQuery.each(json['list'],function(){
-				table.append('<tr><th>'+this[0]+'</th><td>'+this[1]+'</td></tr>');
+				table.append('<tr><th>'+this[0]+'</th><td>'+addFigure(this[1])+'</td></tr>');
 			});
 		});
 	});
