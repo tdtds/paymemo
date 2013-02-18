8ef1a0561022f441c3d040a4b8fac6c663486cff
TADA Tadashi <t@tdtds.jp>
2013-02-18 16:16:00 +0900

    refine javascript

--- public/js/paymemo.js
+++ public/js/paymemo.js
@@ -5,51 +5,43 @@
  You can modify and distribue this under GPL.
  */
 
-function addFigure(str) {
-	var num = new String(str).replace(/,/g, "");
-	while(num != (num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
-	return num;
-}
-
-function addNewItem(db) {
-}
-
-/*
- initialize
- */
 $(function(){
-	$('.paymemo').each(function(){
-		var e = this;
-		var f = $(e).children('form');
-		var db = $(e).attr('id');
-		$(e).children('h2').text(db);
-		f.bind('submit',function(){
-			var item = $('div#'+db+' .paymemo-item')[0].value;
-			var amount = $('div#'+db+' .paymemo-amount')[0].value;
+	function addFigure(str) {
+		var num = new String(str).replace(/,/g, "");
+		while(num != (num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
+		return num;
+	}
+
+	$('div.paymemo').each(function(){
+		var $div = $(this);
+		var $form = $('form', $div);
+		var wallet = $div.attr('id');
+
+		$form.on('submit', function(){
 			$.ajax({
 				type: 'POST',
-				url: paymemoAPI,
-				data: 'item='+item+'&amount='+amount+'&db='+db,
-				dataType: 'json',
-				success: function(json) {
-					var caption = $('div#'+db+' caption');
-					var item = json['list'][0];
-					caption.html('Total: '+addFigure(json['total']));
-					caption.after('<tr><th>'+item[0]+'</th><td>'+item[1]+'</td></tr>');
-					$('div#'+db+' .paymemo-item')[0].value = '';
-					$('div#'+db+' .paymemo-amount')[0].value = '';
-					$('div#'+db+' .paymemo-item')[0].focus();
-				}
+				url: '/' + wallet,
+				data: $form.serialize(),
+				dataType: 'json'
+			}).done(function(json){
+				var caption = $('caption', $div);
+				var item = json['list'][0];
+				caption.html('Total: ' + addFigure(json['total']));
+				caption.after('<tr><th>'+item[0]+'</th><td>'+item[1]+'</td></tr>');
+				$('div#'+db+' .paymemo-item')[0].value = '';
+				$('div#'+db+' .paymemo-amount')[0].value = '';
 			});
 			return false;
 		});
-		f.children('input[name=db]').attr('value',db);
-		$.getJSON(paymemoAPI+'?db='+db,function(json){
-			var table = $(e).children('table');
+
+		$('input')[0].focus();
+
+		$.getJSON('/' + wallet, function(json){
+			var table = $('table', $div);
 			table.empty();
-			table.append('<caption>Total: '+addFigure(json['total'])+'</caption>');
-			jQuery.each(json['list'],function(){
-				table.append('<tr><th>'+this[0]+'</th><td>'+addFigure(this[1])+'</td></tr>');
+			table.append('<caption>Total: ' + addFigure(json['total']) + '</caption>');
+			jQuery.each(json['list'], function(){
+				table.append('<tr><th>' + this[0] + '</th><td>' + addFigure(this[1]) + '</td></tr>');
 			});
 		});
 	});
